774d4be88fc5eed94f4550b8dac6e5fa0a261eaa
satriadhikara <satriadhikara@gmail.com>
2024-01-15 21:03:31 +0700

    Add local fonts and material symbols

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,34 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthContextProvider } from "./context/AuthContext";
+import localFont from "next/font/local";
+import "material-symbols";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata = {
-  title: "SmartMediSync",
-  description: "Your companion for health and wellness",
-};
+const madeSans = localFont({
+  src: [
+    {
+      path: "../public/fonts/MADE-Outer-Sans-Regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/MADE-Outer-Sans-Bold.otf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/MADE-Outer-Sans-Light.otf",
+      weight: "300",
+      style: "normal",
+    },
+  ],
+});
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={madeSans.className}>
         <AuthContextProvider>{children}</AuthContextProvider>
       </body>
     </html>
